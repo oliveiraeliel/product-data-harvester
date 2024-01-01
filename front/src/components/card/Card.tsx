b2778b74f574491e5e3cc7ef20035091a1892a21
Eliel Oliveira <eliel.oliveira2004@gmail.com>
2024-01-01 19:32:13 -0300

    feat: card component

--- front/src/components/card/Card.tsx
+++ front/src/components/card/Card.tsx
@@ -1,7 +1,23 @@
+import styles from "./styles.module.css"
 
+interface CardComponentInterface {
+    title: string,
+    imageUrl: string,
+    price: number
+}
 
-const CardComponent = () => {
-    return <>card</>
+const CardComponent = ({ imageUrl, price, title }: CardComponentInterface) => {
+    const formatPrice = (price: number) => {
+        return price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+    }
+
+    return <div className={styles.cardBody}>
+        <div className={styles.productIdentityBox}>
+            <img src={imageUrl} className={styles.productImage} />
+            <span className={styles.title}>{title}</span>
+        </div>
+        <span className={styles.price}>R$ {formatPrice(price)}</span>
+    </div>
 }
 
 export default CardComponent;
--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,5 +1,6 @@
+import "./globals.css"
 import { CardComponent } from "@/components"
 
 export default function MyApp() {
-    return <CardComponent />
+    return <CardComponent imageUrl="https://www.shutterstock.com/image-photo/example-word-written-on-wooden-260nw-1765482248.jpg" title="aaa" price={14123.51} />
 }
